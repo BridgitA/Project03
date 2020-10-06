27bc5271813ebe6010f5aabfbb7c04d3fe2aad3c
Bridgit Arika <bridgitmob@gmail.com>
2020-10-05 20:48:19 -0500

    Updates

---
+++ precache-manifest.04437916c36fc3dce8d983d86e597879.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0269f3505d12202b092ea4d1d77afc",
+    "url": "/Project03/index.html"
+  },
+  {
+    "revision": "8274b1c6cd43667324bb",
+    "url": "/Project03/static/js/2.a66671d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project03/static/js/2.a66671d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d9c882bd78f4809767",
+    "url": "/Project03/static/js/main.c455dbaa.chunk.js"
+  },
+  {
+    "revision": "08c29c98b899924f98f0",
+    "url": "/Project03/static/js/runtime-main.a3cdae2c.js"
+  }
+]);
