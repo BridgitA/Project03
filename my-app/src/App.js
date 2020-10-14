82e8896864777079041b92fa0a65561a61ddecf2
Bridgit Arika <bridgitmob@gmail.com>
2020-10-14 11:37:23 -0500

    created actions, reducers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import AddTodo from './components/addTodo';
-import TodoList from './components/todoList';
-import VisibilityFilter from './components/visibilityFilter';
-import { Provider } from 'react-redux'
-import store from './redux/store'
-
-const filters = ['All', 'Active', 'Completed']
 
 function App() {
   return (
-    <Provider store = {store}>
-    <div className = "App">
-      <h1>Todo-List</h1>
-      <AddTodo />
-      <TodoList todos={[{content: 'Task 1'}, {content: 'Task 2'}]} />
-      <VisibilityFilter filters = {filters} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </Provider>
   );
 }
 
--- my-app/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import { todos, visibilityFilter } from './reducers';
-
-
-export default createStore (combineReducers({todos, visibilityFilter}), 
-window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_())
