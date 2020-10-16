154d5e1ae85fce1aa0b43570594d35f9be692ede
Bridgit Arika <bridgitmob@gmail.com>
2020-10-16 00:48:58 -0500

    working on the form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import AddTodo from './AddTodo';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Grocery shopping'},
+      {id: 2, content: 'Get homework done'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !==id
+    });
+    this.setState({
+      todos
+    }) 
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
+  render() {
+    return(
+      <BrowserRouter>
+      <div className='todo-app container'>
+        <Navbar /><br></br><br></br>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+      </BrowserRouter>
+    )
+  }
+
 }
+
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import showResults from './showResults';
-import ContactForm from './ContactForm';
-import rootReducer from './reducers';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap,min.css'
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-
-const store = createStore(rootReducer)
-
-render(
-  <Provider store = { store }>
-    <div style = {{padding: 15 }}>
-      <h2>Contact Form</h2>
-      <ContactForm onSubmit = {showResults} />
-    </div>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
